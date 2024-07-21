98fc5f3d70f9962b1d754c5c201feb01043596aa
haneul0509 <haneul0509@naver.com>
2024-07-22 03:53:36 +0900

    Second commit

---
+++ src/main/java/com/group/libraryapp/repository/user/UserRepository.java
@@ -0,0 +1,54 @@
+package com.group.libraryapp.repository.user;
+
+import com.group.libraryapp.dto.user.request.UserUpdateRequest;
+import com.group.libraryapp.dto.user.response.UserResponse;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
+public class UserRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserRepository(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    //해당 객체가 있는지 확인하기 위해 id만 필요하므로, UserUpdateRequest가 아닌 id만 받음
+    public boolean isUserNoExist(long id){
+        String readsql = "select *from user where id = ?";
+        return jdbcTemplate.query(readsql, (rs,rowNum) -> 0, id).isEmpty();
+    }
+
+    public void updateUserName(String name, long id){
+        String sql = "update user set name = ? where id = ?";
+        jdbcTemplate.update(sql, name, id);
+    }
+
+
+    public boolean isUserNoExist(String name){
+        String readsql = "select *from user where name = ?";
+        return jdbcTemplate.query(readsql, (rs,rowNum) -> 0, name).isEmpty();
+
+    }
+
+    public void deleteUser(String name){
+        String sql = "delete from user where name = ?";
+        jdbcTemplate.update(sql, name);
+    }
+
+    public void saveUser(String name, Integer age){
+        String sql = "insert into user (name, age) values (?,?)";
+        jdbcTemplate.update(sql, name, age); //위 물음표에 넣어줄 수 있음!
+    }
+
+    public List<UserResponse> getUsers(){
+        String sql = "select *from user";
+        return jdbcTemplate.query(sql, (rs, rowNum) -> {
+            long id = rs.getLong("id"); //rs = result set. 결과. 결과에서 long 타입 변수를 가져오고, 이름은 id다.
+            String name = rs.getString("name");
+            int age = rs.getInt("age");
+            return new UserResponse(id, name, age);
+        });
+    }
+
+}
---
+++ src/main/java/com/group/libraryapp/service/user/UserService.java
@@ -0,0 +1,40 @@
+package com.group.libraryapp.service.user;
+
+import com.group.libraryapp.dto.user.request.UserCreateRequest;
+import com.group.libraryapp.dto.user.request.UserUpdateRequest;
+import com.group.libraryapp.dto.user.response.UserResponse;
+import com.group.libraryapp.repository.user.UserRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
+//컨트롤러가 객체를 직접 변환하지는 않음. 따라서 @RequestBody가 붙지 않음
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(JdbcTemplate jdbcTemplate){
+        userRepository = new UserRepository(jdbcTemplate);
+    }
+    public void saveUser(UserCreateRequest request){
+        userRepository.saveUser(request.getName(), request.getAge());
+    }
+
+    public List<UserResponse> getUsers(){
+        return userRepository.getUsers();
+    }
+
+    public void updateUser(UserUpdateRequest request){
+        if (userRepository.isUserNoExist(request.getId()))
+        userRepository.updateUserName(request.getName(), request.getId());
+    }
+
+    public void deleteUser(String name){
+
+        if (userRepository.isUserNoExist(name)){
+            throw new IllegalArgumentException();
+        }
+
+        userRepository.deleteUser(name);
+    }
+}
